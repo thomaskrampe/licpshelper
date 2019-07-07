4b7f7e19077edd59be07abfcb2095d08405834db
Thomas Krampe <thomas.krampe@myctx.net>
2019-07-07 15:33:42 +0200

    x

--- licpsfs/licpsfs.psm1
+++ licpsfs/licpsfs.psm1
@@ -198,3 +198,42 @@ Function LC_CleanupDirectory {
     }
 } #EndFunction LC_CleanupDirectory
 Export-ModuleMember -Function 'LC_CleanupDirectory'
+
+function LC_IsAdmin {
+    <#
+        .SYNOPSIS
+            LC_IsAdmin
+        .DESCRIPTION
+            Check if the user running this script has admin permissions
+        .EXAMPLE
+            LC_IsAdmin
+        .RETURN
+            $True or $False
+        .NOTES
+            Author        : Thomas Krampe | t.krampe@loginconsultants.de
+            Version       : 1.1
+            Creation date : 26.07.2018 | v0.1 | Initial function
+            Last change   : 26.07.2018 | v1.1 | Release
+           
+            IMPORTANT NOTICE
+            ----------------
+            THIS FUNCTION IS PROVIDED "AS IS" WITHOUT WARRANTIES OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+            ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON- INFRINGEMENT.
+            LOGIN CONSULTANTS, SHALL NOT BE LIABLE FOR TECHNICAL OR EDITORIAL ERRORS OR OMISSIONS CONTAINED 
+            HEREIN, NOT FOR DIRECT, INCIDENTAL, CONSEQUENTIAL OR ANY OTHER DAMAGES RESULTING FROM FURNISHING,
+            PERFORMANCE, OR USE OF THIS SCRIPT, EVEN IF LOGIN CONSULTANTS HAS BEEN ADVISED OF THE POSSIBILITY
+            OF SUCH DAMAGES IN ADVANCE.
+    #>
+     
+    begin {
+    }
+ 
+    process {
+        ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+    }
+     
+    end {
+    }
+     
+} #Endfunction LC_IsAdmin
+Export-ModuleMember -Function 'LC_IsAdmin'
