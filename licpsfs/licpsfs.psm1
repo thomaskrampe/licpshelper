6cf8d421c271e62c7f712308499b1b09bb070c7a
Thomas Krampe <thomas.krampe@myctx.net>
2019-07-11 08:24:16 +0200

    Add more functions

--- licpsfs/licpsfs.psm1
+++ licpsfs/licpsfs.psm1
@@ -551,6 +551,140 @@ Function LC_InstallOrUninstallSoftware {
 Export-ModuleMember -Function 'LC_InstallOrUninstallSoftware'
 #endregion
 
+#region begin LOGFILES
+Function LC_ClearEventLogs() {
+    <#
+        .SYNOPSIS
+        Clear all event logs
+        .DESCRIPTION
+        Clear all event logs
+        .EXAMPLE
+        LC_ClearEventLogs
+        Loops through all event logs on the local system and clears (deletes) all entries in each of the logs founds
+    #>
+    [CmdletBinding()]
+    Param( 
+    )
+
+    begin {
+        [string]$FunctionName = $PSCmdlet.MyInvocation.MyCommand.Name
+        Write-Verbose "START FUNCTION - $FunctionName"
+    }
+ 
+    process {
+        # Retrieve all event logs on the current system
+        $EventlogList = Get-EventLog -List
+
+        Foreach ($EventLog in $EventlogList) {
+            $EventLogName = $EventLog.Log
+            try {
+                Clear-EventLog -LogName $EventLogName | Out-Null
+            }
+            catch {
+                Write-Host "An error occurred trying to clear the event log $EventLogName (error: $($Error[0]))!"
+                Exit 1
+            }
+        }
+    }
+ 
+    end {
+        Write-Verbose "END FUNCTION - $FunctionName"
+    }
+}
+
+Function LC_WriteToEventLog {
+    <#
+        .SYNOPSIS
+        Write an entry into the Windows event log. New event logs as well as new event sources are automatically created.
+        .DESCRIPTION
+        Write an entry into the Windows event log. New event logs as well as new event sources are automatically created.
+        .PARAMETER EventLog
+        This parameter contains the name of the event log the entry should be written to (e.g. Application, Security, System or a custom one)
+        .PARAMETER Source
+        This parameter contains the source (e.g. 'MyScript')
+        .PARAMETER EventID
+        This parameter contains the event ID number (e.g. 3000)
+        .PARAMETER Type
+        This parameter contains the type of message. Possible values are: Information | Warning | Error
+        .PARAMETER Message
+        This parameter contains the event log description explaining the issue
+        .Example
+        LC_WriteToEventLog -EventLog "System" -Source "MyScript" -EventID "3000" -Type "Error" -Message "An error occurred"
+        Write an error message to the System event log with the source 'MyScript' and event ID 3000. The unknown source 'MyScript' is automatically created
+        .Example
+        LC_WriteToEventLog -EventLog "Application" -Source "Something" -EventID "250" -Type "Information" -Message "Information: action completed successfully"
+        Write an information message to the Application event log with the source 'Something' and event ID 250. The unknown source 'Something'�is automatically created
+        .Example
+        LC_WriteToEventLog -EventLog "MyNewEventLog" -Source "MyScript" -EventID "1000" -Type "Warning" -Message "Warning. There seems to be an issue"
+        Write an warning message to the event log called 'MyNewEventLog' with the source 'MyScript' and event ID 1000. The unknown event log�'MyNewEventLog' and�source 'MyScript'�are automatically created
+    #>
+    [CmdletBinding()]
+    Param( 
+        [parameter(mandatory = $True)]  
+        [ValidateNotNullorEmpty()]
+        [String]$EventLog,
+        [parameter(mandatory = $True)]  
+        [ValidateNotNullorEmpty()]
+        [String]$Source,
+        [parameter(mandatory = $True)]
+        [Int]$EventID,
+        [parameter(mandatory = $True)]
+        [ValidateNotNullorEmpty()]
+        [String]$Type,
+        [parameter(mandatory = $True)]
+        [ValidateNotNullorEmpty()]
+        [String]$Message
+    )
+ 
+    begin {
+        [string]$FunctionName = $PSCmdlet.MyInvocation.MyCommand.Name
+        Write-Verbose "START FUNCTION - $FunctionName" $LogFile
+    }
+ 
+    process {
+        # Check if the event log exist. If not, create it.
+        if ( !( [System.Diagnostics.EventLog]::Exists( $EventLog ) ) ) {
+            try {
+                New-EventLog -LogName $EventLog -Source $EventLog
+            }
+            catch {
+                Write-Host "An error occurred trying to create the event log '$EventLog' (error: $($Error[0]))!"
+            }
+        }
+        else {
+            Write-Verbose "The event log '$EventLog' already exists."
+        }
+
+        # Check if the event source exist. If not, create it.
+        if ( !( [System.Diagnostics.EventLog]::SourceExists( $Source ) ) ) {
+            try {
+                [System.Diagnostics.EventLog]::CreateEventSource( $Source, $EventLog )	
+            }
+            catch {
+                Write-Host "An error occurred trying to create the event source '$Source' (error: $($Error[0]))!"
+            }
+        }
+        else {
+            Write-Verbose "The event source '$Source' already exists."
+        }
+        		
+        # Write the event log entry
+        try {
+            Write-EventLog -LogName $EventLog -Source $Source -eventID $EventID -EntryType $Type -message $Message
+        }
+        catch {
+            Write-Host "An error occurred trying to write the event log entry (error: $($Error[0]))!"
+        }
+    }
+ 
+    end {
+        Write-Verbose "END FUNCTION - $FunctionName"
+    }
+}
+
+
+#endregion
+
 #region OTHER FUNCTIONS
 Function LC_IsAdmin {
     <#
