1a47c7d30169aa4f3ce6e2e7d0c4830bb02b7efa
Thomas Krampe <thomas.krampe@myctx.net>
2021-09-03 12:46:53 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,7 +1,7 @@
 $Name = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
-Set-Content -Path C:\test.txt -Value $Name
+Set-Content -Path C:\temp\test.txt -Value $Name
 
 $Trigger = New-ScheduledTaskTrigger -AtLogon
 $User = "NT AUTHORITY\SYSTEM" 
-$Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "C:\test.ps1" 
+$Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "C:\temp\test.ps1" 
 Register-ScheduledTask -TaskName "JustMyTest" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest -Force
